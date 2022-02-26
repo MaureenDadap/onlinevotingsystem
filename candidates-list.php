a6ac201fcee062ce5d6907dda6028b6779bb1a72
MaureenDadap <maureendadap@gmail.com>
2022-02-26 12:10:31 +0800

    added error pages for voting page access

--- candidates-list.php
+++ candidates-list.php
@@ -10,8 +10,42 @@ include('common/website_info.php');
 
 <body>
     <?= navbar("candidates"); ?>
-    <main></main>
-    <?=footer()?>  
+    <header class="results py-3">
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-md-3">
+                    <img src="images/sammy-25.png" alt="img" class="w-100">
+                </div>
+                <div class="col-md-3">
+                    <h1>Candidates List</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 ">
+                    <div class="card p-4">
+                        <h4>One Partylist</h4>
+                        <span><strong>President: </strong>Jane Doe</span>
+                        <span><strong>Vice President: </strong>Jane Doe</span>
+                        <span><strong>Secretary: </strong>Jane Doe</span>
+                        <span><strong>Treasurer: </strong>Jane Doe</span>
+                        <strong>Representatives: </strong>
+                        <ul>
+                            <li>Jane Doe</li>
+                            <li>Jane Doe</li>
+                            <li>Jane Doe</li>
+                            <li>Jane Doe</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?= footer() ?>
     <script src="js/bootstrap.bundle.js"></script>
 </body>
+
 </html>
--- voting.php
+++ voting.php
@@ -10,32 +10,67 @@ include('common/website_info.php');
 
 <body>
     <?= navbar("voting"); ?>
-    <header class="results py-3">
-        <div class="container">
-            <div class="row justify-content-center align-items-center">
-                <div class="col-md-3">
-                    <img src="images/sammy-25.png" alt="img" class="w-100">
+    <?php
+    if (isset($_SESSION['user_type'])) {
+        if ($_SESSION['user_type'] === 'admin') { ?>
+            <main>
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h1>You do not have access to this page.</h1>
+                            <h5>Only students can vote.</h5>
+                        </div>
+                        <div class="col">
+                            <img src="images/sammy-17.png" alt="error" class="w-100">
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <h1>Voting</h1>
+            </main>
+        <?php
+        } else { ?>
+            <header class="results py-3">
+                <div class="container">
+                    <div class="row justify-content-center align-items-center">
+                        <div class="col-md-3">
+                            <img src="images/sammy-25.png" alt="img" class="w-100">
+                        </div>
+                        <div class="col-md-3">
+                            <h1>Voting</h1>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </header>
-    <main>
-        <div class="container">
-            <h3>President</h3>
-            <div class="row row-cols-1 row-cols-md-4 g-4">
-                <div class="card w-">
-                    <img src="images/college-student-budget.jpg" class="img-fluid card-img-top" alt="candidate">
-                    <div class="card-body">
-                        <h5 class="card-title">Jane Doe</h5>
-                        <p class="card-text">I am going to make this university great again</p>
+            </header>
+            <main>
+                <div class="container">
+                    <h3>President</h3>
+                    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                        <div class="card candidate p-4">
+                            <img src="images/college-student-budget.jpg" class="candidate-img" alt="candidate">
+                            <div class="card-body">
+                                <h5 class="card-title">Jane Doe</h5>
+                                <p class="card-text">I am going to make this university great again</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        <?php }
+    } else { ?>
+        <main>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h1>You do not have access to this page.</h1>
+                        <h5>Log in as a student to vote.</h5>
+                    </div>
+                    <div class="col">
+                        <img src="images/sammy-17.png" alt="error" class="w-100">
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
+        </main>
+    <?php } ?>
+
     <?= footer() ?>
     <script src="js/bootstrap.bundle.js"></script>
     <script>
